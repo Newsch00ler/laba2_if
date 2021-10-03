a419647bd34b2eed3055918de0e9bcddfce85dc4
Newsch00ler <86545418+Newsch00ler@users.noreply.github.com>
2021-10-03 21:12:04 +0800

    Добавьте файлы проекта.

---
+++ WinFormsAppDereza_laba2_IF/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace WinFormsAppDereza_laba2_IF
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textA = new System.Windows.Forms.TextBox();
+            this.textB = new System.Windows.Forms.TextBox();
+            this.textC = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(315, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "#11. Даны вещественные положительные числа a, b, c. ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(291, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Если существует треугольник со сторонами a, b, c, ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(272, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "то определить, является ли он прямоугольным.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "a:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(172, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "b:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(332, 100);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(16, 15);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "c:";
+            // 
+            // textA
+            // 
+            this.textA.Location = new System.Drawing.Point(34, 97);
+            this.textA.Name = "textA";
+            this.textA.Size = new System.Drawing.Size(100, 23);
+            this.textA.TabIndex = 6;
+            // 
+            // textB
+            // 
+            this.textB.Location = new System.Drawing.Point(195, 97);
+            this.textB.Name = "textB";
+            this.textB.Size = new System.Drawing.Size(100, 23);
+            this.textB.TabIndex = 7;
+            // 
+            // textC
+            // 
+            this.textC.Location = new System.Drawing.Point(354, 97);
+            this.textC.Name = "textC";
+            this.textC.Size = new System.Drawing.Size(100, 23);
+            this.textC.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(442, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Определить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 168);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textC);
+            this.Controls.Add(this.textB);
+            this.Controls.Add(this.textA);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Задача на IF";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textA;
+        private System.Windows.Forms.TextBox textB;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textC;
+    }
+}
+
---
+++ WinFormsAppDereza_laba2_IF/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsAppDereza_laba2_IF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            textA.Text = Properties.Settings.Default.a.ToString();
+            textB.Text = Properties.Settings.Default.b.ToString();
+            textC.Text = Properties.Settings.Default.c.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float a = float.Parse(this.textA.Text);
+            float b = float.Parse(this.textB.Text); 
+            float c = float.Parse(this.textC.Text);
+
+            Properties.Settings.Default.a = a;
+            Properties.Settings.Default.b = b;
+            Properties.Settings.Default.c = c;
+            Properties.Settings.Default.Save();
+
+            MessageBox.Show(Logic.Math(a, b, c));
+        }
+    }
+
+    public class Logic
+    {
+        public static string Math(float a, float b, float c)
+        {
+            string message = "Треугольник не существует";
+            if ((a < b + c) || (b < a + c) || (c < a + b))
+            {
+                if ((a * a == b * b + c * c) || (b * b == a * a + c * c) || (c * c == a * a + b * b))
+                {
+                    message = "Прямоугольный треугольник существует";
+                }
+                else
+                {
+                    message = "Прямоугольный треугольник не существует";
+                }
+            }
+            return message;
+        }
+    }
+}
